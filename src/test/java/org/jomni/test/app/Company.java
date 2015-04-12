05fde7dd377046cbeaac2ec0c096a78efe39eebd
jeremychone <jeremy.chone@gmail.com>
2015-04-11 18:29:27 -0700

    . minor addition

---
+++ src/test/java/org/jomni/test/app/Company.java
@@ -0,0 +1,16 @@
+package org.jomni.test.app;
+
+public class Company {
+
+	private String name;
+
+	public Company() {}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
